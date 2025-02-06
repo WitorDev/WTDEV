520e69229becc32345c0e923d98a6c9f66be47bb
Witor Tenã <witortena@gmail.com>
2025-02-06 01:05:22 -0300

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About"
-import Header from "./components/header"
+import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Tech from "./components/Tech"
