0fc79edd622e7fbcb234b915369151108ac989da
Witor <witortena@gmail.com>
2025-07-17 17:30:40 -0300

    fixed rollup error

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ["react-scroll"],
+  },
   plugins: [react(), tailwindcss()],
   build: {
-    rollupOptions: {
-      external: ['react-scroll']
-    }
+    // rollupOptions: {
+    //   external: ['react-scroll']
+    // },
+    commonjsOptions: {
+      include: [/react-scroll/, /node_modules/],
+    },
   }
+
 })
